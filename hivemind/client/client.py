05e425cdb0e7c78a5eef0a829d16425e84bb19d8
fluffydolphin <baxterhoey@gmail.com>
2022-09-30 14:04:42 +1300

    changing ip's so it can work on joe wins server

--- hivemind/client/client.py
+++ hivemind/client/client.py
@@ -20,7 +20,7 @@ client_color = random.choice(colors)
 # server's IP address
 # if the server is not on this machine, 
 # put the private (network) IP address (e.g 192.168.1.2)
-SERVER_HOST = "192.168.3.149"
+SERVER_HOST = "192.168.3.10"
 SERVER_PORT = 420 # server's port
 separator_token = "<SEP>" # we will use this to separate the client name & message
 
