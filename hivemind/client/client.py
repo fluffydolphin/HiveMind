63bd598609ef63da5e59357d0d625276b807a765
fluffydolphin <baxterhoey@gmail.com>
2022-10-01 03:16:09 +1300

    adding command for slowloris help

--- hivemind/client/client.py
+++ hivemind/client/client.py
@@ -44,6 +44,31 @@ while True:
 
 type anything to use HiveMind
 """)
+    if '!slowloris!' in to_send:
+        print("""
+Slowloris, low bandwidth stress test tool for websites
+
+positional arguments:
+  host                  Host to perform stress test on
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -p PORT, --port PORT  Port of webserver, usually 80
+  -s SOCKETS, --sockets SOCKETS
+                        Number of sockets to use in the test
+  -v, --verbose         Increases logging
+  -ua, --randuseragents
+                        Randomizes user-agents with each request
+  -x, --useproxy        Use a SOCKS5 proxy for connecting
+  --proxy-host PROXY_HOST
+                        SOCKS5 proxy host
+  --proxy-port PROXY_PORT
+                        SOCKS5 proxy port
+  --https               Use HTTPS for the requests
+  --sleeptime SLEEPTIME
+                        Time to sleep between each header sent.
+  --duration DURATION   Duration of attack.
+""")
     else: s.send(to_send.encode())
 
 s.close()
